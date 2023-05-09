64d240cfdf45e739335e0a798ba960e24c10a189
usiozero <swortaartonlineforever@gmai.com>
2023-05-09 19:17:35 +0200

    roter start

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.use(express.json());
 // access static data
 app.use(express.static(path.join(__dirname, '/public')));
 
+// router
+app.use('/', require('./routes/root'));
+
+// 404
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type('txt').send("404 Not Found");
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
