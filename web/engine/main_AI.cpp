714bbde784e34d3ee2e2713b6ab85e70c38b3f9d
Erdoganishe <Khotyanmisha@gmail.com>
2023-05-20 16:59:12 +0300

    Main for AI added

---
+++ web/engine/main_AI.cpp
@@ -0,0 +1,68 @@
+#include "PositionRepresentation/Position.cpp"
+#include "PositionRepresentation/Move.cpp"
+#include "MoveGeneration/MoveList.cpp"
+#include "MoveGeneration/LegalMoveGeneration.cpp"
+#include "AI/AI.cpp"
+
+int main()
+{
+
+    std::string get_short_fen;
+    int en_passant;
+    bool w_l_castling;
+    bool w_s_castling;
+    bool b_l_castling;
+    bool b_s_castling;
+    float move_ctr;
+
+    std::cin >> get_short_fen;
+    std::cin >> en_passant;
+    // std::cout << unsigned(std::uint8_t(en_passant));
+    std::cin >> w_l_castling;
+    std::cin >> w_s_castling;
+    std::cin >> b_l_castling;
+    std::cin >> b_s_castling;
+    std::cin >> move_ctr;
+
+    const std::string short_fen = get_short_fen;
+    Position position(short_fen, unsigned(std::uint8_t(en_passant)), w_l_castling, w_s_castling, b_l_castling, b_s_castling, move_ctr);
+
+    bool side;
+    int intPart = static_cast<int>(move_ctr);
+    if (std::abs(move_ctr - intPart) < 0.0001)
+    {
+
+        side = 0;
+    }
+    else
+    {
+        side = 1;
+    }
+    // std::cout << position;
+    //  std::cout<<short_fen;
+    //  std::cout<<en_passant;
+    //  std::cout<<w_l_castling;
+    //  std::cout<<w_s_castling;
+    //  std::cout<<b_l_castling;
+    //  std::cout<<b_s_castling;
+    //  std::cout<<move_ctr;
+
+    AI ai("opening_book.txt");
+    Move bm = ai.best_move(position, side, 1000, 3500);
+    position.move(bm);
+    side = !side;
+    std::cout << position.pieces.getShortFEN() << '\n';
+    std::cout << unsigned(position.en_passant) << '\n';
+    std::cout << position.w_l_castling << '\n';
+    std::cout << position.w_s_castling << '\n';
+    std::cout << position.b_l_castling << '\n';
+    std::cout << position.b_s_castling << '\n';
+    std::cout << position.move_ctr << '\n';
+    MoveList legalmoves = LegalMoveGeneration::generate(position, side, false);
+    for (int i = 0; i < legalmoves.size(); i++)
+    {
+        std::cout << unsigned(legalmoves[i].from) << " " << unsigned(legalmoves[i].to) << " " << unsigned(legalmoves[i].flag) << '\n';
+    }
+
+    return 0;
+}
