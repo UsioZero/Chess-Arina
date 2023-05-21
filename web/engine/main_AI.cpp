1821bf9d1f88bd2f7b6cf0d4d763e82481adfe57
Erdoganishe <khotyanmisha@gmail.com>
2023-05-21 19:12:56 +0300

    Maybe now it will work

--- web/engine/main_AI.cpp
+++ web/engine/main_AI.cpp
@@ -4,7 +4,7 @@
 #include "MoveGeneration/LegalMoveGeneration.cpp"
 #include "AI/AI.cpp"
 
-int main(int argc,const char *argv[])
+int main(int argc, const char *argv[])
 {
 
     std::string get_short_fen;
@@ -22,21 +22,21 @@ int main(int argc,const char *argv[])
     int ds;
     int flag;
 
-    get_short_fen = argv[0];
-    en_passant = atoi(argv[1]);
+    get_short_fen = std::string(argv[1]);
+    en_passant = atoi(argv[2]);
     // std::cout << unsigned(std::uint8_t(en_passant));
-    w_l_castling = argv[2];
-    w_s_castling = argv[3];
-    b_l_castling = argv[4];
-    b_s_castling = argv[5];
-    move_ctr = atof(argv[6]);
-    to = atoi(argv[7]);
+    w_l_castling = argv[3];
+    w_s_castling = argv[4];
+    b_l_castling = argv[5];
+    b_s_castling = argv[6];
+    move_ctr = atof(argv[7]);
     from = atoi(argv[8]);
-    at = atoi(argv[9]);
-    as = atoi(argv[10]);
-    dt = atoi(argv[11]);
-    ds = atoi(argv[12]);
-    flag = atoi(argv[13]);
+    to = atoi(argv[9]);
+    at = atoi(argv[10]);
+    as = atoi(argv[11]);
+    dt = atoi(argv[12]);
+    ds = atoi(argv[13]);
+    flag = atoi(argv[14]);
     const std::string short_fen = get_short_fen;
     Position position(short_fen, unsigned(std::uint8_t(en_passant)), w_l_castling, w_s_castling, b_l_castling, b_s_castling, move_ctr);
 
