6359b6461ba1accebeeedd67dd6a05bc2001544f
pugach22 <usiookazakiprivate@gmail.com>
2023-05-25 18:25:23 +0300

    Settings menu nav added

--- public/scripts/settings/script2.js
+++ public/scripts/settings/script2.js
@@ -1,53 +1,190 @@
-window.addEventListener('DOMContentLoaded', function () {
-    // Define the dataArray with the image URLs
-    var dataArray = ["img/avatars/Avatar7.png", "img/bgs/bg1.png"];
+window.addEventListener('DOMContentLoaded', async function () {
+  // Define the dataArray with the image URLs
+  var dataArray = ["img/avatars/Avatar7.png", "img/bgs/bg1.png"];
 
-    // Get a reference to the img element inside the td element with class "avatar-td"
-    const avatarImgs = document.querySelectorAll('.avatar-td img');
+  // Get a reference to the img element inside the td element with class "avatar-td"
+  const avatarImgs = document.querySelectorAll('.avatar-td img');
 
-    // loop through each img element and set its src attribute to dataArray[0]
-    avatarImgs.forEach(img => {
-        img.src = dataArray[0];
-    });
+  // loop through each img element and set its src attribute to dataArray[0]
+  avatarImgs.forEach(img => {
+    img.src = dataArray[0];
+  });
 
-    // Get a reference to the div element with class "bg-test-image"
-    var bgTestImage = document.querySelector('.bg-test-image');
+  // Get a reference to the div element with class "bg-test-image"
+  var bgTestImage = document.querySelector('.bg-test-image');
 
-    // Set the background image of the div element to the second URL in the dataArray
-    bgTestImage.style.backgroundImage = `url(${dataArray[1]})`;
+  // Set the background image of the div element to the second URL in the dataArray
+  bgTestImage.style.backgroundImage = `url(${dataArray[1]})`;
 
-    const buttonUploadAvatar = document.getElementById("button-upload-avatar");
-    const fileUpload = document.getElementById("file-upload");
+  const buttonUploadAvatar = document.getElementById("button-upload-avatar");
+  const fileUpload = document.getElementById("file-upload");
 
-    buttonUploadAvatar.addEventListener("click", () => {
-        fileUpload.click();
-    });
+  buttonUploadAvatar.addEventListener("click", () => {
+    fileUpload.click();
+  });
 
-    fileUpload.addEventListener("change", (event) => {
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          const avatarImg = document.querySelector(".avatar-td img");
-          avatarImg.src = reader.result;
-          dataArray[0] = reader.result;
-        };
+  fileUpload.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const avatarImg = document.querySelector(".avatar-td img");
+      avatarImg.src = reader.result;
+      dataArray[0] = reader.result;
+    };
+  });
+  const buttonUploadBg = document.getElementById("button-upload-bg");
+  const fileUploadbg = document.getElementById("file-upload-bg");
+
+  buttonUploadBg.addEventListener("click", () => {
+    fileUploadbg.click();
+  });
+
+  fileUploadbg.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const bgImg = document.querySelector(".bg-test-image");
+      bgImg.style.backgroundImage = `url(${reader.result})`;
+      dataArray[1] = reader.result;
+    };
+  });
+
+  const responce = await fetch('/api/user');
+  const resData = await responce.json();
+  const menuImg = document.querySelector("#btn1-container div img");
+  menuImg.src = `img/profiles/${resData._id}/Avatar.png`;
+
+
+
+  //start game modal
+  // Get the modal
+  var modal = document.getElementById("modal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("open-modal");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  let startGameData = [];
+
+  const sideImgs = document.querySelectorAll(".side-img");
+  sideImgs.forEach(sideImg => { sideImg.addEventListener('click', () => { sideImgs.forEach(sideImg2 => sideImg2.classList.remove("active-option")); sideImg.classList.add("active-option") }) });
+  const startGameButton = document.querySelector(".start-game-button");
+
+
+  const typeImgs = document.querySelectorAll(".type-img");
+  const divs = document.querySelectorAll(".choose-slider-div");
+  divs.forEach(div => {
+    div.style.height = "0";
+    div.style.border = "0px";
+    div.style.fontSize = "0px";
+  });
+  divs[0].style.height = "40px";
+  divs[0].style.border = "2px solid #8A4726";
+  divs[0].style.fontSize = "30px";
+  typeImgs.forEach((typeImg, i) => {
+    typeImg.addEventListener('click', () => {
+      typeImgs.forEach(typeImg2 => typeImg2.classList.remove("active-option"));
+      typeImg.classList.add("active-option");
+
+      divs.forEach(div => {
+        div.style.height = "0";
+        div.style.border = "0px";
+        div.style.fontSize = "0px";
+        startGameButton.innerHTML = "Start Game"
       });
-      const buttonUploadBg = document.getElementById("button-upload-bg");
-      const fileUploadbg = document.getElementById("file-upload-bg");
-  
-      buttonUploadBg.addEventListener("click", () => {
-          fileUploadbg.click();
-      });
-  
-      fileUploadbg.addEventListener("change", (event) => {
-          const file = event.target.files[0];
-          const reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = () => {
-            const bgImg = document.querySelector(".bg-test-image");
-            bgImg.style.backgroundImage = `url(${reader.result})`;
-            dataArray[1] = reader.result;
-          };
-        });
+      divs[i].style.height = "40px";
+      divs[i].style.border = "2px solid #8A4726";
+      divs[i].style.fontSize = "30px";
+      if (i == 1) { startGameButton.innerHTML = "Get Link" }
+
+    });
+  });
+  function checkFormat(str) {
+    const regex = /^\d+\|\d+$/;
+    return regex.test(str);
+  }
+
+  const uniqueTimer = document.querySelector(".custom-timer-button");
+  const inp = document.querySelector(".hidden-input");
+  uniqueTimer.addEventListener('click', () => {
+    if (checkFormat(inp.value)) { uniqueTimer.innerHTML = inp.value; } else { uniqueTimer.innerHTML = "Wrong format"; }
+  });
+  const timers = document.querySelectorAll(".default-timer-button");
+  timers.forEach(timer => { timer.addEventListener('click', () => { timers.forEach(timer2 => timer2.classList.remove("active-option")); timer.classList.add("active-option") }) });
+
+
+
+  startGameButton.addEventListener('click', () => {
+    const activeButtons = document.querySelectorAll(".active-option");
+    activeButtons.forEach((button, index) => {
+
+      if (index == 0) {
+        const src = button.getAttribute("src");
+        let fileName = src.substring(src.lastIndexOf("/") + 1);
+        if (fileName == "computericon.png") {
+          fileName = "AI"
+        }
+        else {
+          if (fileName == "human.png") {
+            fileName = "hu";
+          }
+          else {
+            fileName = "hs";
+          }
+
+        }
+        startGameData.push(fileName);
+      }
+      if (index == 1) {
+        const src = button.getAttribute("src");
+        let fileName = src.substring(src.lastIndexOf("/") + 1);
+        if (fileName == "K.png") {
+          fileName = "w";
+        }
+        else {
+          if (fileName == "K2.png") {
+            fileName = "b";
+          }
+          else {
+            fileName = "r";
+          }
+        }
+        startGameData.push(fileName);
+      }
+    });
+    const thirdButton = activeButtons[2];
+    let thirdButtonInnerHTML;
+    if (thirdButton.innerHTML != "Wrong format") {
+      thirdButtonInnerHTML = thirdButton.innerHTML;
+    } else {
+      thirdButtonInnerHTML = "15|10";
+    }
+    startGameData.push(thirdButtonInnerHTML);
+
+    console.log(startGameData);
+    const link = document.querySelector(".start-game-button a");
+    link.href = "http://localhost:3000/game";
+    link.click();
+  });
 });
--- public/scripts/settings/script4.js
+++ public/scripts/settings/script4.js
@@ -1,77 +1,4 @@
-document.addEventListener("DOMContentLoaded",async function () {
-    let isValidCountry = true;
-    let dataArray = ["Іво Бобул", "ibobyl", "Ukraine", "Chernivtsi Oblast", "", ""];
-    let inputName = document.getElementById("input-name");
-    inputName.value = dataArray[0];
-    let inputNick = document.getElementById("input-nick");
-    inputNick.value = "@" + dataArray[1];
-    let inputCountry = document.getElementById("input-country");
-    inputCountry.value = dataArray[2];
-    let inputState = document.getElementById("input-state");
-    inputState.value = dataArray[3];
-    let inputCity = document.getElementById("input-city");
-    if (dataArray[4] != "") {
-        inputCity.value = dataArray[4];
-    }
-    else {
-        inputCity.value = "(Do not display)"
-    }
-    let inputPremium = document.getElementById("input-premium");
-    if (dataArray[5] != "") {
-        inputPremium.value = dataArray[5];
-    }
-    else {
-        inputPremium.value = "(Not owned)";
-    }
-    // Get a list of all country names using the REST Countries API
-    fetch("https://restcountries.com/v2/all")
-        .then(response => response.json())
-        .then(data => {
-            let countryNames = data.map(country => country.name);
-
-            // Add an event listener to the input element with ID "input-country"
-            let inputCountry = document.getElementById("input-country");
-            inputCountry.addEventListener("input", function () {
-                let inputValue = inputCountry.value.trim();
-
-                // Check if the input value is a valid country name
-                if (countryNames.includes(inputValue)) {
-                    inputCountry.style.color = "black";
-                    isValidCountry = true;
-                    if (inputValue == "Russian Federation") { inputCountry.value = "Москаль, вийшов нахуй"; isValidCountry = false; }
-                } else {
-                    inputCountry.style.color = "red";
-                    isValidCountry = false;
-                }
-            });
-        })
-        .catch(error => console.error(error));
-
-
-        let saveButton = document.querySelector(".save-button");
-
-        // Add a click event listener to the "Save" button
-        saveButton.addEventListener("click", function() {
-          // Get the values of the input fields and save them to the dataArray array
-            dataArray[0] =  inputName.value;
-            if (isValidCountry){
-            dataArray[2] =  inputCountry.value;
-            }
-            else{
-                alert("Wrong country, your changes discarded");
-            }
-            dataArray[3] =  inputState.value;
-            if (inputCity.value != "(Do not display)"){
-                dataArray[4] =  inputCity.value;
-            }
-            else
-            {
-                dataArray[4] =  "";
-            }
-          // Log the updated dataArray array to the console
-          console.log(dataArray);
-        });
-
+document.addEventListener('DOMContentLoaded', async function () {
     const responce = await fetch('/api/user');
     const resData = await responce.json();
     const menuImg = document.querySelector("#btn1-container div img");
@@ -208,5 +135,6 @@ document.addEventListener("DOMContentLoaded",async function () {
         link.href = "http://localhost:3000/game";
         link.click();
     });
-        
+
 });
+
