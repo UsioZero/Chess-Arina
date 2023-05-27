850265df8c1d6fa517420fcf4f94d56a368d0a6e
usiozero <swortaartonlineforever@gmai.com>
2023-05-27 04:10:17 +0200

    missed data 2

--- middleware/fileSaver.js
+++ middleware/fileSaver.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const fileSaver = (req, res) => {
     const file = req.file;
     console.log(file);
-    const uId = req.body.jsonData.uId;
-    const isAvatar = req.body.jsonData.isAvatar;
+    const uId = SON.parse(req.body.jsonData).uId; // may be JSON.parse() is nit necessary
+    const isAvatar = JSON.parse(req.body.jsonData).isAvatar; // may be JSON.parse() is nit necessary
 
     fs.mkdirSync(path.join(__dirname, '../public/img/profiles/', uId));
     const filePath = path.join(__dirname, '../public/img/profiles', uId, (isAvatar ? "avatar" : "bg") + path.extname(file));
