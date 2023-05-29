e3887b339d1aebea39e0fa2f10f26b736ecd4400
usioZero <swortaartonlineforever@gmail.com>
2023-05-30 00:30:16 +0200

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://chess-arena.herokuapp.com/', // Change
+    'https://chess-arena-3.onrender.com', // Change
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'http://192.168.0.1:3000'];
