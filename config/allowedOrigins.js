20c41cb3cb2786079745d05fba4c68e2d3bf7b7e
usiozero <swortaartonlineforever@gmai.com>
2023-05-29 20:53:47 +0200

    set website name

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://www.DOMAINNAME.com', // Change
+    'https://chess-arena.herokuapp.com/', // Change
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'http://192.168.0.1:3000'];
